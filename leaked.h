6e1c4d9e6707ea0e0398eb232c2fc6e0afdd5c9d
ali. <alielmalki.developer@gmail.com>
2025-10-01 01:48:56 +0000

    refactoring

--- leaked.h
+++ leaked.h
@@ -1,16 +1,38 @@
 /*
- *                   LEAKED.H
+ *                     LEAKED.H
  *
-    Header-only memory leak detector for C .  It trackes
-    allocations/frees, warns on double frees, and shows
-    memory alive and leaks at program exit. also it supports
-    multi-file projects with a single global tracker.
-
-
-Copyright (c) 2023, 2024, 2025 Ali Almalki
-Distributed under the MIT License
-
-*/
+ *    Simple memory leak detector for C. It tracks
+ *    allocations/frees, warns on double frees/invalid use,
+ *    and shows memory alive and leaks at program exit.
+ *    Also supports multi-file projects with a single global tracker.
+ *
+ *    Copyright (c) 2023, Ali Almalki, All rights reserved.
+ *                  (BSD-3-Clause)
+ *
+ *    EXAMPLE USAGE:
+ *
+ *        // main.c :
+ *
+ *        // must define this macro in one file (i.e main)
+ *        #define LEAKED_IMPLEMENTATION
+ *        #include "leaked.h"
+ *        
+ *        int main(void)
+ *        {
+ *            leaked_init();         // call once at start
+ *        
+ *            int* a = malloc(10);   // tracked
+ *            int* b = calloc(5, 1); // tracked
+ *            a = realloc(a, 20);    // tracked
+ *        
+ *            free(b); // free 'b'
+ *            free(b); // double-free !
+ *            // forgot to free 'a' !
+ *        
+ *            return 0;
+ *        }
+ *
+ */
 
 #ifndef LEAKED_H
 #define LEAKED_H 1
@@ -20,64 +42,62 @@ Distributed under the MIT License
 #include <stdio.h>
 #include <stdlib.h>
 
-/* ANSI colors for terminal output */
 #define RED "\033[31m"
 #define GRN "\033[32m"
 #define YEL "\033[33m"
 #define RESET "\033[0m"
 
-/* One node per allocation */
 typedef struct Blk
 {
-    void* ptr;        /* pointer returned by malloc/calloc/realloc */
-    size_t sz;        /* bytes allocated */
-    const char* file; /* file where allocated */
-    int line;         /* line number of allocation */
+    void* ptr;
+    size_t sz;
+    const char* file;
+    int line;
+    const char* type;
     struct Blk* next;
 } Blk;
 
-/* Manager for tracking all allocations */
 typedef struct Mgr
 {
-    Blk* head;          /* linked list of active allocations */
-    long allocs;        /* total allocations */
-    long frees;         /* total frees */
-    size_t bytes_alloc; /* total bytes allocated */
-    size_t bytes_free;  /* total bytes freed */
-    long alive;         /* current live blocks */
+    Blk* head;
+    long allocs;
+    long frees;
+    size_t bytes_alloc;
+    size_t bytes_free;
+    long alive;
 } Mgr;
 
-/* Singleton manager instance */
+/* Singleton global instance manager */
 #ifdef LEAKED_IMPLEMENTATION
 Mgr mgr = { NULL, 0, 0, 0, 0, 0 };
 #else
 extern Mgr mgr;
 #endif
 
-/* add a new allocation to tracking */
-static inline void add_blk(void* p, size_t sz, const char* f, int l)
+static inline void _add_blk(void* p,
+                            size_t sz,
+                            const char* f,
+                            int l,
+                            const char* t)
 {
     if (!p)
         return;
-
     Blk* b = (Blk*)malloc(sizeof(*b));
     if (!b)
         return;
-
     b->ptr = p;
     b->sz = sz;
     b->file = f;
     b->line = l;
+    b->type = t;
     b->next = mgr.head;
     mgr.head = b;
-
     mgr.allocs++;
     mgr.alive++;
     mgr.bytes_alloc += sz;
 }
 
-/* remove allocation from tracking */
-static inline bool del_blk(void* p, const char* f, int l)
+static inline bool _del_blk(void* p, const char* f, int l)
 {
     Blk** pp = &mgr.head;
     while (*pp) {
@@ -86,47 +106,48 @@ static inline bool del_blk(void* p, const char* f, int l)
             *pp = tmp->next;
             mgr.bytes_free += tmp->sz;
             free(tmp);
-
             mgr.frees++;
             mgr.alive--;
             return true;
         }
         pp = &(*pp)->next;
     }
-
-    /* pointer not found => double free or invalid free */
     fprintf(stderr, YEL "[WARN]" RESET " invalid free %p at %s:%d\n", p, f, l);
     return false;
 }
 
-/* print report at exit */
+/* Print leak report to stderr */
 static inline void show_leaks(void)
 {
-    int leaks = 0;
+    if (!mgr.head) {
+        fprintf(stderr,
+                GRN "Leaked allocations:\n  none\nTotal leaked: 0 bytes in 0 "
+                    "allocation\n" RESET);
+        return;
+    }
+    fprintf(stderr, RED "Leaked allocations:\n" RESET);
+    int idx = 1;
+    size_t total_bytes = 0;
     for (Blk* b = mgr.head; b; b = b->next) {
         fprintf(stderr,
-                RED "[LEAK]" RESET " %p (%zu bytes) at %s:%d not freed\n",
+                YEL "  %d) " RESET "ptr=%p size=%zu allocated at %s:%d (%s)\n",
+                idx++,
                 b->ptr,
                 b->sz,
                 b->file,
-                b->line);
-        leaks++;
+                b->line,
+                b->type);
+        total_bytes += b->sz;
     }
-
-    if (leaks == 0)
-        fprintf(stderr, GRN "[LEAKED] no leaks detected\n");
-    else
-        fprintf(stderr,
-                YEL "[LEAKED] %d leak(s), allocs=%ld frees=%ld alive=%ld ;; "
-                    "%zu bytes alive\n" RESET,
-                leaks,
-                mgr.allocs,
-                mgr.frees,
-                mgr.alive,
-                mgr.bytes_alloc - mgr.bytes_free);
+    fprintf(stderr,
+            RED "Total leaked: " RESET "%zu bytes in %d allocation%s\n",
+            total_bytes,
+            idx - 1,
+            (idx - 1 > 1 ? "s" : ""));
 }
 
-/* register atexit handler once */
+/* Initialize leak tracker and register show_leaks() to run at program exit
+ * automatically */
 static inline void leaked_init(void)
 {
     static bool done = false;
@@ -136,59 +157,63 @@ static inline void leaked_init(void)
     }
 }
 
-/* malloc wrapper */
-static inline void* xmalloc(size_t n, const char* f, int l)
+static inline void* _xmalloc(size_t n, const char* f, int l)
 {
     void* p = malloc(n);
-    add_blk(p, n, f, l);
+    _add_blk(p, n, f, l, "malloc");
     return p;
 }
 
-/* calloc wrapper */
-static inline void* xcalloc(size_t n, size_t s, const char* f, int l)
+static inline void* _xcalloc(size_t n, size_t s, const char* f, int l)
 {
     void* p = calloc(n, s);
-    add_blk(p, n * s, f, l);
+    _add_blk(p, n * s, f, l, "calloc");
     return p;
 }
 
-/* realloc wrapper */
-static inline void* xrealloc(void* old, size_t n, const char* f, int l)
+static inline void* _xrealloc(void* old, size_t n, const char* f, int l)
 {
     void* p = realloc(old, n);
+    if (!p)
+        return NULL;
 
-#if defined(__GNUC__) || defined(__clang__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wuse-after-free"
-#endif
-    if (old && old != p)
-        del_blk(old, f, l);
-#if defined(__GNUC__) || defined(__clang__)
-#pragma GCC diagnostic pop
-#endif
+    if (old) {
+        if (old != p) {
+            _del_blk(old, f, l);
+            _add_blk(p, n, f, l, "realloc");
+        } else {
+            for (Blk* b = mgr.head; b; b = b->next) {
+                if (b->ptr == old) {
+                    b->sz = n;
+                    b->file = f;
+                    b->line = l;
+                    b->type = "realloc";
+                    break;
+                }
+            }
+        }
+    } else {
+        _add_blk(p, n, f, l, "realloc");
+    }
 
-    if (p)
-        add_blk(p, n, f, l);
     return p;
 }
 
-/* free wrapper */
-static inline void xfree(void* p, const char* f, int l)
+static inline void _xfree(void* p, const char* f, int l)
 {
     if (!p)
         return;
-    if (del_blk(p, f, l))
+    if (_del_blk(p, f, l))
         free(p);
 }
 
-/* --- Replace standard API --- */
 #undef malloc
 #undef calloc
 #undef realloc
 #undef free
-#define malloc(n) xmalloc(n, __FILE__, __LINE__)
-#define calloc(n, s) xcalloc(n, s, __FILE__, __LINE__)
-#define realloc(p, n) xrealloc(p, n, __FILE__, __LINE__)
-#define free(p) xfree(p, __FILE__, __LINE__)
+#define malloc(n) _xmalloc(n, __FILE__, __LINE__)
+#define calloc(n, s) _xcalloc(n, s, __FILE__, __LINE__)
+#define realloc(p, n) _xrealloc(p, n, __FILE__, __LINE__)
+#define free(p) _xfree(p, __FILE__, __LINE__)
 
 #endif /* LEAKED_H */
